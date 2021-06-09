f0c9dfbdb95cd3dad339608d08de249c1344b428
benoitgaujac <benoit.gaujac@gmail.com>
2021-06-09 09:15:53 +0100

    added module networks

---
+++ networks.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow.compat.v1 as tf
+from math import ceil, sqrt
+
+from ops import linear, non_linear
+
+import logging
+import pdb
+
+def mlp(opts, input, output_dim, nlayers, init=None, stddev=0.0099999, bias=0., nonlinear='leaky_relu', alpha=1., scope=None, reuse=False):
+    layer_x = input
+    with tf.variable_scope(scope, reuse=reuse):
+        for i in range(nlayers-1):
+            layer_x = linear(opts, layer_x, np.prod(layer_x.get_shape().as_list()[1:]),
+                        output_dim=2, init=init,
+                        stddev=bias, bias=bias,
+                        scope='hid{}/lin'.format(i),
+                        reuse=reuse)
+            layer_x = non_linear(layer_x, nonlinear, alpha)
+    # output layer
+    outputs = linear(opts, layer_x, np.prod(layer_x.get_shape().as_list()[1:]),
+                output_dim=output_dim, init=init,
+                stddev=bias, bias=bias,
+                scope='hid_lin_final',
+                reuse=reuse)
+    layer_x = non_linear(layer_x, nonlinear, alpha)
+
+    return layer_x
